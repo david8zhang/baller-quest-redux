e67ad37a7065bc7d72d308787d6b3e607100aea9
David Zhang <david8zhang@berkeley.edu>
2023-01-22 12:59:03 -0800

    update start screen

--- src/scenes/Start.ts
+++ src/scenes/Start.ts
@@ -1,4 +1,5 @@
 import { DEFAULT_FONT, WINDOW_HEIGHT, WINDOW_WIDTH } from '~/core/Constants'
+import { Hoop } from '~/core/Hoop'
 import { button } from '~/ui/Button'
 
 export class Start extends Phaser.Scene {
@@ -7,28 +8,49 @@ export class Start extends Phaser.Scene {
   }
 
   create() {
-    const titleText = this.add.text(0, 0, 'Baller Quest', {
-      fontSize: '75px',
+    this.add.image(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 'court')
+    this.add.image(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 175, 'hoop-stand').setScale(5)
+    this.add.image(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 175, 'hoop-rim').setScale(5)
+    this.add.rectangle(
+      WINDOW_WIDTH / 2,
+      WINDOW_HEIGHT / 2,
+      WINDOW_WIDTH,
+      WINDOW_HEIGHT,
+      0x000000,
+      0.1
+    )
+
+    const titleText = this.add.text(0, 0, 'Baller', {
+      fontSize: '200px',
       fontFamily: DEFAULT_FONT,
       color: 'white',
     })
-    titleText.setPosition(WINDOW_WIDTH / 2 - titleText.displayWidth / 2, WINDOW_HEIGHT / 2.5)
+    titleText.setPosition(WINDOW_WIDTH / 2 - titleText.displayWidth / 2, WINDOW_HEIGHT / 6.25)
+    titleText.setStroke('#000000', 20)
+
+    const titleText2 = this.add.text(0, 0, 'Quest', {
+      fontSize: '200px',
+      fontFamily: DEFAULT_FONT,
+      color: 'white',
+    })
+    titleText2.setPosition(WINDOW_WIDTH / 2 - titleText2.displayWidth / 2, WINDOW_HEIGHT / 2.5)
+    titleText2.setStroke('#000000', 20)
 
     const startButton = button('Play', {
       fontFamily: DEFAULT_FONT,
-      fontSize: '20px',
+      fontSize: '50px',
       color: 'black',
-      width: 150,
-      height: 40,
-      borderWidth: '2px',
+      width: 300,
+      height: 80,
+      borderWidth: '5px',
       borderStyle: 'solid',
       borderColor: 'black',
-      borderRadius: '10px',
+      borderRadius: '25px',
       cursor: 'pointer',
     }) as HTMLElement
 
     const startButtonDOM = this.add
-      .dom(this.scale.width / 2, titleText.y + 125, startButton)
+      .dom(this.scale.width / 2, titleText2.y + 250, startButton)
       .setOrigin(0.5)
       .addListener('click')
       .on('click', () => {
